3682059e018514af8bdeee6b9cb4a37c396be321
Vaibhav lodha <vlodha98@gmail.com>
2020-07-31 17:36:27 +0530

    Kingdom Tests

--- src/test/java/com/project/tameofthrones/model/KingdomTest.java
+++ src/test/java/com/project/tameofthrones/model/KingdomTest.java
@@ -1,35 +1,88 @@
 package com.project.tameofthrones.model;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.eq;
 
 import com.project.tameofthrones.conversions.Cipher;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class KingdomTest {
 
   @Mock
-  Kingdom kingdom;
+  Cipher cipher;
 
   @InjectMocks
-  Cipher cipher;
+  Kingdom kingdom;
+
+  int[] requiredCipher = new int[26];
+
+  int[] translatedMessage = new int[26];
+
+  @BeforeEach
+  public void init() {
+    MockitoAnnotations.initMocks(this);
+    requiredCipher[3] = 1;
+    requiredCipher[4] = 1;
+    requiredCipher[5] = 1;
+
+    translatedMessage[3] = 1;
+    translatedMessage[4] = 1;
+    translatedMessage[5] = 1;
+  }
 
   @Test
   void makeCipherFromAnimalName() {
 
+    Mockito.when(cipher.encodeAnimalNameToCipher(eq("ABC")))
+        .thenReturn(requiredCipher);
+
+    kingdom.setEmblem("AIR");
+    kingdom.setAnimal("ABC");
+
+    kingdom.makeCipherFromAnimalName();
+
+    assertArrayEquals(requiredCipher, kingdom.getRequiredCipher());
+    assertEquals(1, kingdom.getRequiredCipher()[3]);
   }
 
   @Test
   void sendMessageToKingdom() {
 
+
+    Mockito.when(cipher.generateCipherFromMessage(eq("DEF")))
+        .thenReturn(translatedMessage);
+
+    kingdom.sendMessageToKingdom("DEF");
+
+    assertArrayEquals(translatedMessage, kingdom.getTranslatedMessage());
+    assertEquals(1, kingdom.getTranslatedMessage()[4]);
   }
 
   @Test
   void isKingdomAlliedToRuler() {
+
+    Mockito.when(cipher.encodeAnimalNameToCipher(eq("ABC")))
+        .thenReturn(requiredCipher);
+
+    Mockito.when(cipher.generateCipherFromMessage(eq("DEF")))
+        .thenReturn(translatedMessage);
+
+    Mockito.when(cipher.matchCipher(eq(requiredCipher), eq(translatedMessage)))
+        .thenReturn(Boolean.TRUE);
+
+    kingdom.setAnimal("ABC");
+    kingdom.sendMessageToKingdom("DEF");
+    kingdom.makeCipherFromAnimalName();
+
+    assertTrue(kingdom.isKingdomAlliedToRuler());
+
   }
 }
